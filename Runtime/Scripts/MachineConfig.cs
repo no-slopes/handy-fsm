e43410e1436329e55d59286bd7167e10d1c78834
Gabo <indiegabodev@gmail.com>
2023-10-29 14:29:43 -0300

    feat(PropertyDrawer): created the property drawer for the MachineConfig property of the StateMachine

---
+++ Runtime/Scripts/MachineConfig.cs
@@ -0,0 +1,13 @@
+
+using UnityEngine;
+
+namespace HandyFSM
+{
+    [System.Serializable]
+    public class MachineConfig
+    {
+        [SerializeField]
+        private float _configTest;
+    }
+
+}
