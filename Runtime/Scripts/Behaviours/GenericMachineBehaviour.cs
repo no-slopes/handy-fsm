7e5048206d9bb5a563bfa6b2028cc9be7008e6e9
Indie Gabo <indiegabodev@gmail.com>
2023-12-19 12:23:36 -0300

    Fix: StateMachineBehaviour not loading default scriptable state

--- Runtime/Scripts/Behaviours/GenericMachineBehaviour.cs
+++ Runtime/Scripts/Behaviours/GenericMachineBehaviour.cs
@@ -14,11 +14,24 @@ namespace HandyFSM
         {
             _stateProvider.LoadStatesFromBaseType(typeof(TBaseState), false);
             _defaultState = _stateProvider.Get(typeof(TDefaultState));
+        }
 
-            // Initialize all the states
-            // This should occur after the states have been loaded so they can recognize each other
-            // when using Machine.GetState<>();
-            _stateProvider.InitializeAllStates();
+        #endregion
+    }
+
+    /// <summary>
+    /// The state machine
+    /// </summary>
+    public abstract class GenericMachineBehaviour<TBaseState> : StateMachineBehaviour
+    {
+        #region Machine Engine
+
+        /// <summary>
+        /// This method recognizes and initializes the states for the machine.
+        /// </summary>
+        protected void BeforeInitialized()
+        {
+            _stateProvider.LoadStatesFromBaseType(typeof(TBaseState), false);
         }
 
         #endregion
