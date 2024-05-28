1a9308e68fd9a98d9c176f86e280e2e809359c07
Indie Gabo <indiegabodev@gmail.com>
2024-05-28 16:46:04 -0300

    Removing Odin Inspector

--- Assets/Tests/Inheritance/Child.cs
+++ Assets/Tests/Inheritance/Child.cs
@@ -1,11 +1,9 @@
 using System;
 using IndieGabo.HandyFSM.Implementations;
-using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class Child : GenericHandyFSMBrain<ChildState, ChildStateIdle>
 {
-    [BoxGroup("Test")]
     [SerializeField]
     private float _testFloat;
 }
