61b57aae9aeabbaaa300b55969d8cb4f21df4356
Indie Gabo <indiegabodev@gmail.com>
2024-11-07 15:28:16 -0300

    fix: Triggers now can be dataless

--- Assets/HandyFSM/Runtime/Scripts/Core/Triggers/TriggersProvider.cs
+++ Assets/HandyFSM/Runtime/Scripts/Core/Triggers/TriggersProvider.cs
@@ -7,43 +7,87 @@ namespace IndieGabo.HandyFSM
     public class TriggersProvider
     {
         protected FSMBrain _brain;
-        protected Dictionary<string, UnityEvent<TriggerData>> _triggers = new();
+        protected Dictionary<string, List<UnityAction>> _datalessTriggers = new();
+        protected Dictionary<string, List<UnityAction<TriggerData>>> _dataTriggers = new();
 
         public TriggersProvider(FSMBrain brain)
         {
             _brain = brain;
         }
 
-        public void Squeeze(string key, TriggerData data = null)
+        public void Squeeze(string key)
         {
-            if (!_triggers.ContainsKey(key))
+            if (!_datalessTriggers.ContainsKey(key))
             {
                 Debug.LogError($"Trying to squeeze trigger '{key}' but it does not exist for StateMachine Brain \"{_brain.name}\"");
                 return;
             }
 
-            _triggers[key].Invoke(data);
+            foreach (UnityAction action in _datalessTriggers[key])
+            {
+                action?.Invoke();
+            }
+        }
+
+        public void Squeeze(string key, TriggerData value)
+        {
+            if (!_dataTriggers.ContainsKey(key))
+            {
+                Debug.LogError($"Trying to squeeze trigger '{key}' but it does not exist for StateMachine Brain \"{_brain.name}\"");
+                return;
+            }
+
+            foreach (UnityAction<TriggerData> action in _dataTriggers[key])
+            {
+                action?.Invoke(value);
+            }
+        }
+
+        public void RegisterCallback(string key, UnityAction action)
+        {
+            if (!_datalessTriggers.ContainsKey(key))
+            {
+                _datalessTriggers.Add(key, new List<UnityAction>());
+            }
+
+            _datalessTriggers[key].Add(action);
+        }
+
+        public void UnregisterCallback(string key, UnityAction action)
+        {
+            if (!_datalessTriggers.ContainsKey(key)) return;
+
+            var list = _datalessTriggers[key];
+
+            list.Remove(action);
+
+            if (list.Count == 0)
+            {
+                _datalessTriggers.Remove(key);
+            }
         }
 
         public void RegisterCallback(string key, UnityAction<TriggerData> action)
         {
-            if (!_triggers.ContainsKey(key))
+            if (!_dataTriggers.ContainsKey(key))
             {
-                _triggers.Add(key, new UnityEvent<TriggerData>());
+                _dataTriggers.Add(key, new List<UnityAction<TriggerData>>());
             }
 
-            _triggers[key].AddListener(action);
+            _dataTriggers[key].Add(action);
         }
 
         public void UnregisterCallback(string key, UnityAction<TriggerData> action)
         {
-            if (!_triggers.ContainsKey(key))
-            {
-                Debug.LogError($"Trying to unregister callback for trigger '{key}' but it does not exist for StateMachine Brain \"{_brain.name}\"");
-                return;
-            }
+            if (!_dataTriggers.ContainsKey(key)) return;
 
-            _triggers[key].RemoveListener(action);
+            var list = _dataTriggers[key];
+            list.Remove(action);
+
+            if (list.Count == 0)
+            {
+                _dataTriggers.Remove(key);
+            }
         }
     }
 }
